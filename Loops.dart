84432935318306f4451c38a5b65684facdb8b30a
m_aqsam <maqsam1155@gmail.com>
2024-01-04 01:20:47 +0500

    Upload lopps in dart file .

---
+++ Loops.dart
@@ -0,0 +1,27 @@
+void main() {
+  // ***************** Loops In dart *****************
+
+  // * For Loop
+
+  for (int i = 1; i <= 5; i++) {
+    print('Iteration $i');
+  }
+
+  // * While Loop
+
+  int count = 1;
+
+  while (count <= 5) {
+    print('Value is  $count');
+    count++;
+  }
+
+  // * D0-While Loop
+
+  int num = 1;
+
+  do {
+    print("Your num is $num");
+    num++;
+  } while (num <= 5);
+}
