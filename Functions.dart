0dc7fb23cc5a60175c41650372b3801ade363ed0
m_aqsam <maqsam1155@gmail.com>
2024-01-07 23:38:18 +0500

    Update functions.dart

--- Functions.dart
+++ Functions.dart
@@ -1,6 +1,21 @@
 void main() {
   printName();
   print(greet("Aqsam"));
+
+  print(printValues()); // (12,Aqsam)
+
+  // or
+  final (age, name) = printValues();
+  print(age); // 12
+  print(name); // Aqsam
+
+  // 2nd Way to use Records n Functions
+
+  final values = Print_Data();
+  print(values.age);
+  print(values.String);
+
+  print(Print_Data());
 }
 
 // Functions
@@ -16,3 +31,20 @@ void printName() {
 String greet(String name) {
   return 'Hello, $name!';
 }
+
+// *******************  Records in Functions  *******************
+
+(int, String) printValues() {
+  return (12, "Aqsam");
+}
+
+// Another more reliable way to do that in order to avoid error or mistakes
+
+({String String, int age}) Print_Data() {
+  return (age: 12, String: "Aqsam");
+}
+
+// Quick about Functions 
+
+// Instead of using {} braces you can use => in functions 
+//Recommended whrn when you have return single value 
