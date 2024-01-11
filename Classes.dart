c3019a20979000e3efa62bd5e6b301f24e315610
m_aqsam <maqsam1155@gmail.com>
2024-01-12 00:37:00 +0500

    Update class , and inheritance touch

--- Classes.dart
+++ Classes.dart
@@ -17,14 +17,32 @@ class Person {
   }
 }
 
-class Person2 {
-  person2(String sp) {
-    print("$sp I am a constructor of Person2");
+class Base {
+  Base() {
+    print("I am a Constructor of Base ");
+  }
+  Base.witha(int a) {
+    print("I am a Constructor of Base with $a");
+  }
+}
+
+// Inheritance Touch
+
+class Derived extends Base {
+  Derived() {
+    print("I am a Constructor of Derived");
+  }
+  Derived.witha(int a) {
+    print("I am a Constructor of Derived with $a");
   }
 }
 
 void main() {
   Person person1 = Person(); // Object of Person1
-  Person2 obj1 = Person2(); // Object of Person2
   person1.print_name("Hello");
+
+  Base obj1 = Base();
+  Base obj2 = Base.witha(4);
+
+  Derived obj3 = Derived(); // first call base constructor then derive const
 }
