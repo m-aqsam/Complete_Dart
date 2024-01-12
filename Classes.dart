d6e96399931761953fc88eba19543c60527a11f5
m_aqsam <maqsam1155@gmail.com>
2024-01-13 00:09:56 +0500

    Update class (private variables ,setter and getters )

--- Classes.dart
+++ Classes.dart
@@ -24,6 +24,25 @@ class Base {
   Base.witha(int a) {
     print("I am a Constructor of Base with $a");
   }
+
+  // private Variables
+
+  int _height =
+      4; // an _ use with the variable name to assign it as a private variable
+  int _width =
+      5; // private variables only access and modifies in same class and modifies using setters and getters
+
+  // setters and Getters
+
+  // Setter to acces or read only value of height using setter
+
+  int get height => _height;
+
+  // Getter to modify the value of height
+
+  set setheight(int h) {
+    _height = h;
+  }
 }
 
 // Inheritance Touch
@@ -38,12 +57,16 @@ class Derived extends Base {
 }
 
 void main() {
-  Person person1 = Person(); // Object of Person1
-  person1.print_name("Hello");
+  //Person person1 = Person(); // Object of Person1
+  //person1.print_name("Hello");
 
   Base obj1 = Base();
-  Base obj2 = Base.witha(4);
+  //Base obj2 = Base.witha(4);
 
-  Derived obj3 = Derived(); // first call base constructor then derive const
-  Derived obj4 = Derived();
+  //Derived obj3 = Derived(); // first call base constructor then derive const
+  //Derived obj4 = Derived();
+
+  print(obj1.height);
+  obj1.setheight = 10; // update private variable value
+  print(obj1.height);
 }
