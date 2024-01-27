af33080cf887f375787b32ee07e074c0c49fe6ba
m_aqsam <maqsam1155@gmail.com>
2024-01-27 23:53:17 +0500

    add collection file

---
+++ Collections/Collections.dart
@@ -0,0 +1,24 @@
+import 'dart:collection';
+import 'dart:ffi';
+
+void main(List<String> args) {
+  
+List<int> numbers = [1, 2, 3, 4, 5];
+
+Set<String> fruits = {'apple', 'banana', 'orange'};
+
+Map<String, int> ages = {'Alice': 25, 'Bob': 30, 'Charlie': 22};
+
+
+// Other Collections
+
+
+// Queue<int> queue = Queue.from([1, 2, 3]);
+// LinkedList<String> linkedList = LinkedList<String>();
+// HashMap<String, double> prices = HashMap<String, double>();
+
+
+
+
+
+}
