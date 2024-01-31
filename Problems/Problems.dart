dcae1608e3e3d7072378c330bfa658cad249a5e2
m_aqsam <maqsam1155@gmail.com>
2024-01-31 23:57:32 +0500

    Update Problems .dart

---
+++ Problems/Problems.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  // Here are some problems for Practice and Revisions Of all Related Topics and Concept in Dart
+
+  print("*****************  (1)  *****************");
+
+  // Write a Dart program to declare variables of different data types
+  //(int, double, String) and perform basic arithmetic operations on them.
+
+  int num1 = 5;
+  double num2 = 2.5;
+  String text = "Dart Programming";
+
+  print("Sum: ${num1 + num2}");
+  print("Text Length: ${text.length}");
+}
