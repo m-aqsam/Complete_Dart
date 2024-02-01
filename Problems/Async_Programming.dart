04114e18527e92d823c537737574667e953a4cf1
m_aqsam <maqsam1155@gmail.com>
2024-02-01 23:57:58 +0500

    problems update

--- Problems/Async_Programming.dart
+++ Problems/Async_Programming.dart
@@ -1,3 +1,8 @@
+// Problem
+
+// Develop a Dart program that uses Future and async/await to simulate asynchronous tasks.
+//For example, fetching data from a fake API and processing it asynchronously.
+
 void main() async {
   print("Start");
 
